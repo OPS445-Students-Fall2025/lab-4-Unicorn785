3c1ec1746a9295162cb192c54408821f400e1564
Pirajeen Kandasamy <pkandasamy7@myseneca.ca>
2025-10-02 11:36:12 -0400

    lab4a comparing sets

--- lab4a.py
+++ lab4a.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
+# Author ID: pkandasamy7
 
 def join_sets(s1, s2):
-    #Return a set containing every value from both s1 and s2.
-    return s1.union(s2)
+    # join_sets will return a set that contains every value from both s1 and s2
+    return s1 | s2
 
 def match_sets(s1, s2):
-    #Return a set containing all values found in both s1 and s2.
-    return s1.intersection(s2)
+    # match_sets will return a set that contains all values found in both s1 and s2
+    return s1 & s2
 
 def diff_sets(s1, s2):
-   #Return a set containing values NOT shared between the sets
-    return s1.symmetric_difference(s2)
+    # diff_sets will return a set that contains all different values which are not shared between the sets
+    return s1 ^ s2
 
-#mainblock, setting up the 2 sets with the rance, and printing
 if __name__ == '__main__':
     set1 = set(range(1,10))
     set2 = set(range(5,15))
