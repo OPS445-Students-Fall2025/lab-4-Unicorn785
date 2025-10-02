52d61db9bd7314820afb2b703ef65078b5691336
Pirajeen Kandasamy <pkandasamy7@myseneca.ca>
2025-10-02 11:15:01 -0400

    lab4d.py manipulating strings with patterns

--- lab4d.py
+++ lab4d.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Strings 1
+# Author ID: pkandasamy7
 
 str1 = 'Hello World!!'
 str2 = 'Seneca College'
@@ -8,23 +8,18 @@ num1 = 1500
 num2 = 1.50
 
 def first_five(s):
-    #Return the first five characters of a string
     return s[:5]
 
 def last_seven(s):
-    #Return the last seven characters of a string
     return s[-7:]
 
 def middle_number(n):
-    #Return the 2nd and 3rd characters of a number (as string
     s = str(n)
     return s[1:3]
 
 def first_three_last_three(s1, s2):
-    #Return first three chars of s1 and last three chars of s2 concatenated
     return s1[:3] + s2[-3:]
 
-#This is the main block
 if __name__ == '__main__':
     print(first_five(str1))
     print(first_five(str2))
